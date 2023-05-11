1678d64c461f11a9c81531598293b8bab66d1ea9
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-11 09:22:27 +0600

    create bookingCollection and post bookings add

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
     
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings')
 
     app.get('/services', async(req,res) => {
         const cursor = serviceCollection.find();
@@ -45,10 +46,13 @@ async function run() {
             // Include only the `title` and `imdb` fields in the returned document
             projection: { title: 1, price: 1,service_id:1 },
           };
-
-
         const result = await serviceCollection.findOne(query,options);
         res.send(result);
+    })
+
+    app.post('/bookings',(req,res)=> {
+      const booking = req.body;
+
     })
 
 
