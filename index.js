c248ceba602da036751002466ed0a21b83ec43b2
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-11 18:45:05 +0600

    delete specific id bookings

--- index.js
+++ index.js
@@ -44,19 +44,39 @@ async function run() {
         const options = {
            
             // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, price: 1,service_id:1 },
+            projection: { title: 1, price: 1,service_id:1,img:1 },
           };
         const result = await serviceCollection.findOne(query,options);
         res.send(result);
     })
 
-    app.post('/bookings',async(req,res)=> {
+    // bookings
+    app.get('/bookings',async (req,res) => {
+      console.log(req.query.email);
+      let query = {}
+      if(req.query.email){
+        query = {email:req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/bookings', async(req,res)=> {
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
+    })
 
+    app.put('/bookings/:id', async(req,res) => {
+      const updatedBooking = req.body;
+    })
 
+    app.delete('/bookings/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
     })
 
 
